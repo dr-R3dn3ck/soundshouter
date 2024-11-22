ce5b18069440fd8258a1ae71c9788e2dc6ae4e32
Max Gairing <max.gairing@googlemail.com>
2024-11-22 01:42:20 +0100

    initial commit vue webapp

---
+++ soundshouter-vite-webapp/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import SoundTable from "./components/SoundTable.vue"
+import NavBar from "./components/NavBar.vue"
+import Footer from "./components/Footer.vue"
+</script>
+
+<template >
+
+    <header>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <NavBar />
+    </header>
+
+    <body>
+      <div class="container mx-auto">
+        <SoundTable />
+      </div>
+    </body>
+
+    <footer>
+      <Footer />
+    </footer>
+    
+
+</template>
