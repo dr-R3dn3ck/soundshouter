6855475bac3272abeb7c3b95f0e5f2082b36d0c6
Max Gairing <max.gairing@googlemail.com>
2024-12-15 23:46:06 +0100

    added side bar for categories

--- soundshouter-vite-webapp/src/App.vue
+++ soundshouter-vite-webapp/src/App.vue
@@ -6,19 +6,28 @@
     <NavBar />
   </header>
 
-  <body class="px-20 min-h-screen max-h-screen width dark:bg-gray-800">
-    <SoundTable>
-      <SoundElement v-for="sound in sounds" 
-      :id="sound.id" 
-      :name="sound.name"
-      :duration="sound.duration" 
-      :play_count="sound.play_count"
-      :category_id="sound.category_id" 
-      :caterory="sound.category"
-      :subcategory_id="sound.subcategory_id" >
-        
-      </SoundElement>
-    </SoundTable>
+  <body class="min-h-screen max-h-screen dark:bg-gray-800">
+    <SideBar>
+        <template #element>
+            <SideBarElement v-for="category in categories"
+                :name="category.name">
+            </SideBarElement>
+        </template>
+        <template #main>
+            <SoundTable>
+            <SoundElement v-for="sound in sounds" 
+            :id="sound.id" 
+            :name="sound.name"
+            :duration="sound.duration" 
+            :play_count="sound.play_count"
+            :category_id="sound.category_id" 
+            :caterory="sound.category"
+            :subcategory_id="sound.subcategory_id" >
+                
+            </SoundElement>
+            </SoundTable>
+        </template>
+    </SideBar>
   </body>
 
   <footer class="bg-gray-800 shadow dark:bg-gray-800 sticky bottom-0 z-20">
@@ -33,6 +42,8 @@ import SoundTable from "./components/SoundTable.vue"
 import NavBar from "./components/NavBar.vue"
 import Footer from "./components/Footer.vue"
 import SoundElement from "./components/SoundElement.vue"
+import SideBar from "./components/SideBar.vue"
+import SideBarElement from "./components/SideBarElement.vue"
 
 import { reactive } from 'vue'
 import { onMounted } from 'vue'
---
+++ soundshouter-vite-webapp/src/components/SideBar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
+<template>
+
+    <aside id="logo-sidebar"
+        class="fixed top-36 left-0 z-40 w-64 h-screen transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        aria-label="Sidebar">
+        <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+            <ul class="space-y-2 font-medium">
+                <slot name="element"></slot>
+            </ul>
+        </div>
+    </aside>
+
+    <div class="p-4 sm:ml-64">
+        <slot name="main"></slot>
+    </div>
+
+</template>
