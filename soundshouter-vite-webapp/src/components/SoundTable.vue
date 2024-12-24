59adb1a5f38e069e9c5d63896220a3b7b1692965
Max Gairing <max.gairing@googlemail.com>
2024-12-24 01:23:42 +0100

    workaorund for overflow

--- soundshouter-vite-webapp/src/components/SoundTable.vue
+++ soundshouter-vite-webapp/src/components/SoundTable.vue
@@ -11,9 +11,9 @@ onMounted(() => {
 </script>
 
 <template class="">
-        <div class="min-h-screen shadow-md sm:rounded-lg sm:ml-64">
+        <div class="max-lg:overflow-auto min-h-screen shadow-md sm:rounded-lg sm:ml-64">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="sticky top-48 z-20 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead class="max-lg:hidden sticky top-48 z-20 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="sticky top-30 z-20 p-4"> 
                         </th>
@@ -35,9 +35,8 @@ onMounted(() => {
                     </tr>
                 </thead>
 
-                <tbody class="overscroll-y-auto">
+                <tbody>
                     <slot name="soundelements"></slot>
-
                 </tbody>
             </table>
         </div>
