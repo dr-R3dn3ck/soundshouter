79325b0d2833df68c8cada03077a90da41f9c245
Max Gairing <max.gairing@googlemail.com>
2024-12-31 02:30:38 +0100

    changed subcat to footer and adde inject for subcatcall

--- soundshouter-vite-webapp/src/components/Footer.vue
+++ soundshouter-vite-webapp/src/components/Footer.vue
@@ -1,11 +1,11 @@
-<script>
+    <script>
 </script>
 
 <template>
 
     <div class="w-full mx-auto max-w-screen-xl p-8 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href=""
-                class="hover:underline">Soundshouter</a>. All Rights Reserved.
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"><a
+                class="hover:underline">Soundshouter</a>. Crafted for LANs
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
